0abb30c8bd7981b43fcf4a829ec4ec367992b529
xnacro <businessofficialtech@gmail.com>
2026-06-29 23:06:45 +0530

    program to show working of arithematic operators

---
+++ Input and Output/Operators/Arithematic.py
@@ -0,0 +1,13 @@
+# Program to show working of arithematic operators.
+print("Operators is a symbol that is used to perform some type of operations on operands..")
+a,b = 34,56
+
+print("(a + b)", a + b)
+print("(a - b)", a - b)
+print("(a * b)", a * b)
+print("(a / b)", a / b)
+print("(a % b)", a % b)
+print("(a // b)", a // b)
+print("(a ** b)", a ** b)
+
+
