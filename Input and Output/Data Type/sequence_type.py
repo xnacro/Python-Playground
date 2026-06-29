0ddb1fcc1dca945400026911ee37d241d97161f6
xnacro <businessofficialtech@gmail.com>
2026-06-29 22:56:36 +0530

    program to demostrate complete list, tuple and string data type in python

--- Input and Output/Data Type/sequence_type.py
+++ Input and Output/Data Type/sequence_type.py
@@ -17,3 +17,29 @@ print(f"Modified List : {lst}")
 # Adding items
 lst.append("Janhvi")
 print(f"Updated List after append: {lst}")
+
+print("-----------------------------------------")
+# Tuples (Immutable Sequence)
+tpl = (21,"Prince",10.03)
+print(f"Tuple : {tpl}")
+print(f"Element at index 1: {tpl[1]}")
+
+print("Note: Tuples are immutable; their values cannot be altered after creation.")
+
+# String 
+str = "Hello Prince"
+print(f"String Value : {str}")
+print(f"String Negative Indexing : {str[-1]}")
+print(f"String Positive Indexing : {str[0]}")
+
+# String slicing
+print(str[0:5])
+
+# String Reverse
+print("Reversed value: ", str[::-1])
+print("Reversed value: ","".join(reversed(str)))
+
+rev = ""
+for ch in str:
+   rev = ch + rev
+print("Reversed value: ", rev)
