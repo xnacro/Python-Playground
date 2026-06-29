7018a30ab87e3421bdf838b89e2ef9f3acf9ab82
xnacro <businessofficialtech@gmail.com>
2026-06-29 23:29:07 +0530

    program to show the working of relational operators..

---
+++ Input and Output/Operators/Relational.py
@@ -0,0 +1,11 @@
+# Program to show the working of relational operators..
+
+a = 56
+b = 65
+
+print(f"a > b : {a > b}")
+print(f"a < b : {a > b}")
+print(f"a <= b : {a <= b}")
+print(f"a >= b : {a >= b}")
+print(f"a == b : {a == b}")
+print(f"a != b : {a != b}")
