92e0756c10267eb3aaeebea06bcedd0053e85fd3
xnacro <businessofficialtech@gmail.com>
2026-06-29 23:23:02 +0530

    program to show the working of assingment operators..

---
+++ Input and Output/Operators/Assingment.py
@@ -0,0 +1,23 @@
+# Program to show the working of assingment operators..
+
+a = 23
+b = 34
+
+a = b
+print(f"after a + b : {a}")
+
+a += b
+print(f"after a += b : {a}")
+
+a -= b
+print(f"after a -= b : {a}")
+
+a *= b
+print(f"after a *= b : {a}")
+
+a /= b 
+print(f"after a /= b : {a}")
+
+a %= b
+print(f"after a %= b : {a}")
+
