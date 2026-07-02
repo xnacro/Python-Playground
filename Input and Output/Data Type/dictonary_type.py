24b9cd0fdefa27e2b40ba5b2b2cc9182c90bafba
xnacro <businessofficialtech@gmail.com>
2026-07-02 23:37:49 +0530

    program to demonstrate dictionary data types in Python.

---
+++ Input and Output/Data Type/dictonary_type.py
@@ -0,0 +1,10 @@
+# Write a program to demonstrate dictionary data types in Python. 
+
+dict = {
+    'Name': 'John',
+    'Age': 25,
+    'City': 'New York'
+}
+
+print(f"Dictionary Value : {dict} and type : {type(dict)}")
+
